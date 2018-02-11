ff6af5af82d771431af4b8fabbf196fb2e1aa8d0
Arun Mavumkal <arunmavumkal@Aruns-MacBook-Pro.local>
2018-02-10 22:56:02 -0500

    first base-service commit

---
+++ frontend/src/app/services/base-service.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class BaseServiceService {
+
+  private baseUrl = 'https://prv-web.sens.buffalo.edu/apis/';
+  private url: string;
+  constructor() {
+    // variables --------------------------------------------//
+
+  }
+
+}
