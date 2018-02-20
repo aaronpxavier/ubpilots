0282eea48e6dab6ec577577b048af1a1b2d273b1
Arun Mavumkal <arunmavumkal@Aruns-MacBook-Pro.local>
2018-02-20 00:16:40 -0500

    added logbook component to app.module.ts and created routes for logbook

--- frontend/src/app/components/logbook/logbook.component.ts
+++ frontend/src/app/components/logbook/logbook.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser'
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-logbook',
   templateUrl: './logbook.component.html',
   styleUrls: ['./logbook.component.css']
 })
+
 export class LogbookComponent implements OnInit {
 
   constructor(private titleService: Title) {
     this.titleService.setTitle("UBPA Logbook");
   }
-  
+
   ngOnInit() {
   }
 
