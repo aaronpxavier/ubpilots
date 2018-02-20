c2ace995d89be0ded78c72ad4d4d9ffbe288b47d
FelipeCanales94 <f.canales94@gmail.com>
2018-02-19 23:30:15 -0500

    Added title
    Added title

--- frontend/src/app/components/logbook/logbook.component.ts
+++ frontend/src/app/components/logbook/logbook.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-logbook',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogbookComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("UBPA Logbook");
+  }
+  
   ngOnInit() {
   }
 
