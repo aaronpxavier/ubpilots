dbb667d45b1daafdb8bf495b2eef977229f403a9
Arun Mavumkal <arunmavumkal@Aruns-MacBook-Pro.local>
2018-02-22 18:25:48 -0500

    added mods to app component and login component to use new services

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { HideNavMenuService } from './services/parent_comp_controls/hide-nav-menu.service';
+import { HideFooterService } from './services/parent_comp_controls/hide-footer-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +9,10 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-    public constructor(private titleService: Title ) {
+    public constructor(private titleService: Title,
+                       public menuService: HideNavMenuService,
+                       public footerService: HideFooterService) {
         this.titleService.setTitle("Loading..");
     }
-
 
 }
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { HideNavMenuService } from '../../services/parent_comp_controls/hide-nav-menu.service';
+import {HideFooterService} from '../../services/parent_comp_controls/hide-footer-service.service';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,14 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title,
+              public menuService: HideNavMenuService,
+              public footerService: HideFooterService) {
         this.titleService.setTitle("Login");
-  }
+        this.menuService.hide();
+        this.footerService.hide();
+        console.log(this.menuService.getState());
+  }g
 
   ngOnInit() {
   }
